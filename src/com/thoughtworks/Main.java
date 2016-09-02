4979f4e8448c4d25540dc5ceb476c462cbf96d74
iamrivina <rivinakumar@gmail.com>
2016-09-02 17:16:44 +0530

    Update

--- src/com/thoughtworks/Main.java
+++ src/com/thoughtworks/Main.java
@@ -1,24 +1,24 @@
 package com.thoughtworks;
-import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            boolean fizzOrBuzz = false;
-            if (i % 3 == 0) {
-                System.out.print("Fizz");
-                fizzOrBuzz = true;
-            }
-            if (i % 5 == 0) {
-                System.out.print("Buzz");
-                fizzOrBuzz = true;
-            }
+            for (int i = 1; i <= 100; i++) {
+                boolean fizzOrBuzz = false;
+                if (i % 3 == 0) {
+                    System.out.print("Fizz");
+                    fizzOrBuzz = true;
+                }
+                if (i % 5 == 0) {
+                    System.out.print("Buzz");
+                    fizzOrBuzz = true;
+                }
 
-            if (fizzOrBuzz) {
-                System.out.println();
-            } else {
-                System.out.println(String.valueOf(i));
+                if (fizzOrBuzz) {
+                    System.out.println();
+                } else {
+                    System.out.println(String.valueOf(i));
+                }
             }
-        }
     }
 }
